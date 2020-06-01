585b999f29fb56800679f8a40827f7715979af51
bagjaehong <dkxltks25@naver.com>
2020-06-01 18:13:55 +0900

    document number2

--- document/src/index.js
+++ document/src/index.js
@@ -1,6 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
-
-ReactDOM.render(<h1>hello, world!</h1>, document.getElementById("root"));
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello,{formatName(user)}</h1>;
+  }
+  return <h1>Hello,Stranger.</h1>;
+}
+function formatName(user) {
+  return `${user.firstName} ${user.lastName}`;
+}
+const user = {
+  firstName: "Harper",
+  lastName: "Perez",
+};
+const element = { 
+    type: 'h1',
+    props:{
+        className:'greting',
+        children:'Hello, world!'
+    }
+}
+ReactDOM.render(element, document.getElementById("root"));
