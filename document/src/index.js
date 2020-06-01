f003489c38366755d71450b6d11688c1aab8e17a
bagjaehong <dkxltks25@naver.com>
2020-06-01 18:21:49 +0900

    document-3

--- document/src/index.js
+++ document/src/index.js
@@ -1,24 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function getGreeting(user) {
-  if (user) {
-    return <h1>Hello,{formatName(user)}</h1>;
-  }
-  return <h1>Hello,Stranger.</h1>;
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello,world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById("root"));
 }
-function formatName(user) {
-  return `${user.firstName} ${user.lastName}`;
-}
-const user = {
-  firstName: "Harper",
-  lastName: "Perez",
-};
-const element = { 
-    type: 'h1',
-    props:{
-        className:'greting',
-        children:'Hello, world!'
-    }
-}
-ReactDOM.render(element, document.getElementById("root"));
+
+setInterval(tick,1000);
+
