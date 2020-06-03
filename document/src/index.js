6ec6da80a52575fb3d1b35dc480f5f54d35fef54
bagjaehong <dkxltks25@naver.com>
2020-06-03 19:47:21 +0900

    document-10 part2

--- document/src/index.js
+++ document/src/index.js
@@ -1,33 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function BolingVerdict(props) {
-  if (props.celsius >= 100) {
-    return <p> The water would boil.</p>;
-  }
-  return <p>The water would not boil.</p>;
-}
+const scaleNames = {
+  c: "Celsius",
+  f: "Fahrenheit",
+};
 
-class Calculator extends React.Component {
+class TemperatureInput extends React.Component {
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
     this.state = { temperature: "" };
+    this.handleChange = this.handleChange.bind(this);
   }
-  handleChange(event) {
-    this.setState({
-      temperature: event.target.value,
-    });
+  handleChange(e) {
+    this.setState({ temperature: e.target.value });
   }
   render() {
     const temperature = this.state.temperature;
+    const scale = this.props.scale;
     return (
       <fieldset>
-        <legend>Enter temperature in Celsius:</legend>
+        <legend>Enter temperature in {scaleNames[scale]}: </legend>
         <input value={temperature} onChange={this.handleChange} />
-        <BolingVerdict celsius={parseFloat(temperature)} />
       </fieldset>
     );
+  }
+}
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div>
+        <TemperatureInput sacle="c"/>
+        <TemperatureInput sacle="f"/> 
+      </div>
+    )
   }
 }
 ReactDOM.render(<Calculator />, document.getElementById("root"));
