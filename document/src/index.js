b2de1d272d070d4d0186fb30860ba9a3584f3d71
bagjaehong <dkxltks25@naver.com>
2020-06-03 19:15:55 +0900

    document-9 part2

--- document/src/index.js
+++ document/src/index.js
@@ -1,35 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class NameForm extends React.Component {
+class EssayForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
-    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      value: "Please write an essay about your favorite DOM element",
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
   handleChange(event) {
     this.setState({ value: event.target.value });
   }
   handleSubmit(event) {
-    alert("A name was submitted: " + this.state.value);
+    alert("An essay was submitted: " + this.state.value);
     event.preventDefault();
   }
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-          Name:{" "}
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-          <input type="submit" value="Submit" />
+          Essay :{" "}
+          <textarea value={this.state.value} onChange={this.handleChange} />
         </label>
+        <input type="submit" value="submit" />
       </form>
     );
   }
 }
 
-ReactDOM.render(<NameForm />, document.getElementById("root"));
+ReactDOM.render(<EssayForm />, document.getElementById("root"));
