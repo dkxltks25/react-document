9f8565d2be441ee10f5144f6aa808e3b7147c479
bagjaehong <dkxltks25@naver.com>
2020-06-03 19:02:40 +0900

    document-8 part2

--- document/src/index.js
+++ document/src/index.js
@@ -1,19 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const numbers = [1, 2, 3, 4, 5];
-
-function ListItem(props) {
-  return <li>{props.value}</li>;
+function Blog(props) {
+  const sidebar = (
+    <ul>
+      {props.posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
+  const content = props.posts.map((post) => (
+    <div key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  ));
+  return (
+    <div>
+      {sidebar}
+      <hr />
+      {content}
+    </div>
+  );
 }
-
-function NumberList(props) {
-  const numbers = props.numbers;
-  const listItems = numbers.map((number) => <ListItem key={number.toString()} value={number} />);
-  return <ul>{listItems}</ul>;
-}
-
-ReactDOM.render(
-  <NumberList numbers={numbers} />,
-  document.getElementById("root")
-);
+const posts = [
+  {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+  {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+];
+ReactDOM.render(<Blog posts={posts }/>, document.getElementById("root"));
